00bb00510b37ecbbb2555aa75b1ce021d541d7ce
hizumo <hajime0630@mbr.nifty.com>
2024-06-13 22:35:48 +0900

    消火器が移動できるように修正
    消火器が移動できるように修正しました

--- Assets/AppMain/Script/Shooting/ShotSrc.cs
+++ Assets/AppMain/Script/Shooting/ShotSrc.cs
@@ -24,6 +24,11 @@ public class ShotSrc : MonoBehaviour
     [SerializeField] Status DefaultStatus = new Status();
     // 現在のステータス
     public Status CurrentStatus = new Status();
+
+    [SerializeField] private float moveSpeed = 0.1f;
+    [SerializeField] private Vector3 minBounds; // 移動範囲の最小値
+    [SerializeField] private Vector3 maxBounds; // 移動範囲の最大値
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,25 +41,53 @@ public class ShotSrc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        HandleMovement();
+        HandleShooting();
+
+    }
+
+    private void HandleMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical ,0) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement, Space.World);
+
+        // 現在の位置を取得
+        Vector3 clampedPosition = transform.position;
+
+        // X軸の範囲制限
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, minBounds.x, maxBounds.x);
+        // y軸の範囲制限
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, minBounds.y, maxBounds.y);
+        // z軸の範囲制限
+        clampedPosition.z = Mathf.Clamp(clampedPosition.z, minBounds.z, maxBounds.z);
+
+        // 制限された位置を適用
+        transform.position = clampedPosition;
+    }
+
+    private void HandleShooting()
+    {
+        if (Input.GetButtonDown("Fire1"))
         {
-            var bulletInstance = Instantiate<GameObject>(bulletPrefab,muzzle.position,muzzle.rotation*Quaternion.Euler(0, 0, 0));
+            var bulletInstance = Instantiate<GameObject>(bulletPrefab, muzzle.position, muzzle.rotation * Quaternion.Euler(0, 0, 0));
             Debug.Log(muzzle.position);
             Debug.Log(muzzle.rotation.GetType());
-            bulletInstance.GetComponent<Rigidbody>().AddForce(bulletInstance.transform.up*bulletPower);
+            bulletInstance.GetComponent<Rigidbody>().AddForce(bulletInstance.transform.up * bulletPower);
             CurrentStatus.Hp -= WaterPoint;
             // transform.position = Vector3.MoveTowards(transform.position,direction,step);
             FireBar.value = CurrentStatus.Hp;
-            Destroy(bulletInstance,5f);
+            Destroy(bulletInstance, 5f);
         }
 
-        if(CurrentStatus.Hp == 0)
+        if (CurrentStatus.Hp == 0)
         {
             Fire.SetActive(false);
             Smog.SetActive(true);
-            SceneManager.LoadScene( "ConversationScene5" );
+            SceneManager.LoadScene("ConversationScene5");
         }
     }
-
 
 }
