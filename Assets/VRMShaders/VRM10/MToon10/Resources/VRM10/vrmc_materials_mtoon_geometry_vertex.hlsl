20650bf61e047ebfca73ee83247c3d7f2b5a7e44
hizumo <hajime0630@mbr.nifty.com>
2024-06-16 22:41:32 +0900

    Buildができるようになった
    Buildができるようになったよ

--- Assets/VRMShaders/VRM10/MToon10/Resources/VRM10/vrmc_materials_mtoon_geometry_vertex.hlsl
+++ Assets/VRMShaders/VRM10/MToon10/Resources/VRM10/vrmc_materials_mtoon_geometry_vertex.hlsl
@@ -1,4 +1,6 @@
-﻿#ifndef VRMC_MATERIALS_MTOON_GEOMETRY_VERTEX_INCLUDED
+﻿#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+#ifndef VRMC_MATERIALS_MTOON_GEOMETRY_VERTEX_INCLUDED
 #define VRMC_MATERIALS_MTOON_GEOMETRY_VERTEX_INCLUDED
 
 #include "./vrmc_materials_mtoon_render_pipeline.hlsl"
@@ -16,7 +18,7 @@ inline half MToon_GetOutlineVertex_OutlineWidth(const float2 uv)
 {
     if (MToon_IsParameterMapOn())
     {
-        return _OutlineWidth * UNITY_SAMPLE_TEX2D_LOD(_OutlineWidthTex, uv, 0).x;
+        return _OutlineWidth * SAMPLE_TEXTURE2D_LOD(_OutlineWidthTex, sampler_OutlineWidthTex, uv, 0).x;
     }
     else
     {
@@ -26,7 +28,6 @@ inline half MToon_GetOutlineVertex_OutlineWidth(const float2 uv)
 
 inline float MToon_GetOutlineVertex_ScreenCoordinatesWidthMultiplier(const float4 positionCS)
 {
-    // NOTE: VRM 1.0 の仕様にはない実装なので、ユーザが任意で機能を外せるようにする.
     if (MToon_UseStrictMode())
     {
         return 1.0f;
@@ -35,11 +36,7 @@ inline float MToon_GetOutlineVertex_ScreenCoordinatesWidthMultiplier(const float
     {
         const float maxViewFrustumPlaneHeight = 2.0f;
         const float invTangentHalfVerticalFov = unity_CameraProjection[1][1];
-        // NOTE: viewFrustumPlaneHeight = tan(halfFov) * viewDirDistance * 2.0
-        //           -> viewDirDistance = viewFrustumPlaneHeight / (tan(halfFov) * 2.0)
         const float widthScaledMaxDistance = maxViewFrustumPlaneHeight * invTangentHalfVerticalFov * 0.5f;
-        // NOTE: VR などの高視野角カメラでは、純粋な実装では太くなりすぎる.
-        //       よってある距離での視錐台平面が高さ 2m 以上になったらスケールをやめる.
         return min(positionCS.w, widthScaledMaxDistance);
     }
 }
@@ -73,7 +70,6 @@ inline VertexPositionInfo MToon_GetOutlineVertex(const float3 positionOS, const 
         const float clipSpaceHeight = 2.0f;
         normalProjectedCS *= clipSpaceHeight * outlineWidth * MToon_GetOutlineVertex_ScreenCoordinatesWidthMultiplier(positionCS);
         normalProjectedCS.x *= aspect;
-        // NOTE: カメラ方向軸を向く法線を持つ頂点が XY 方向にだけずれると困るので、それを抑制する.
         normalProjectedCS.xy *= saturate(1 - normalVS.z * normalVS.z);
 
         VertexPositionInfo output;
